c6670df3a946cd19cc4d0ef474a3d6fa6141aa71
Ihor Chernomorets <oxnardxvi@gmail.com>
2023-11-28 20:41:37 +0100

    Test completed

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        //a, e, i, o, u—and sometimes y
+        if( character == 'a' || character == 'A' ||
+            character == 'e' || character == 'E' ||
+            character == 'i' || character == 'I' ||
+            character == 'o' || character == 'O' ||
+            character == 'u' || character == 'U' ||
+            character == 'y' || character == 'Y') {
+            System.out.println("Vowel");
+        }
+        else if (character >= 'a' && character <= 'z' || character >= 'A' && character <= 'Z') {
+            System.out.println("Consonant");
+        }
+        else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
